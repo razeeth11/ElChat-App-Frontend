0b6f967be130e3fc104728d7cdebcab89df096e0
razeeth11 <codebyrazeeth@gmail.com>
2026-01-13 22:06:28 +0530

    changes

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:9790", {
+export const socket = io("https://elchat-app-backend.onrender.com", {
   autoConnect: false,
 });
